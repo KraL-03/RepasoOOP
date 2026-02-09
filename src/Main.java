507a1c346422afcc2c8223b6e28f26aed56bc103
Daniel Guzman <danielgmnr@gmail.com>
2026-02-08 19:38:52 -0500

    FINAL MERGE

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        CuentaBancaria cuenta1 = new CuentaBancaria("1", 100);
-        cuenta1.retirar(100);
-        cuenta1.consignar(150);
-        System.out.printf("El saldo de la cuenta es: " + cuenta1.getSaldo());
+
+        Notificacion[] notificaciones = new Notificacion[5];
+
+        notificaciones[0] = new NotificacionCorreo("Pepito");
+        notificaciones[1] = new NotificacionSMS("Juanito");
+        notificaciones[2] = new NotificacionIg("Maria");
+        notificaciones[3] = new NotificacionWhatsApp("Juan Mecanico");
+        notificaciones[4] = new NotificacionClashRoyale("Steven");
+
+        notificaciones[0].enviarMensaje("Hola mi amigo");
+        notificaciones[1].enviarMensaje("Hola mi broco");
+        notificaciones[2].enviarMensaje("Hola desde Instagram");
+        notificaciones[3].enviarMensaje("Bienvenido al grupo de WhatsApp de la clase de mecanica");
+        notificaciones[4].enviarMensaje("¡La batalla te espera en la Arena!");
 
     }
 }
